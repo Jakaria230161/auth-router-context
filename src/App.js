b04e8ca8c3865e215d2480b818a182f19824102b
Jakaria230161 <jakariamahmud016@gmail.com>
2022-10-19 17:17:18 +0600

    login and register button added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { createBrowserRouter,RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-    <h1>Hi Jakaria</h1>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
